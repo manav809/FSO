a7d32c5bbc99473b1dd06a17f330c77a8b2bef57
manav809 <manavpatel6978@gmail.com>
2023-11-03 21:15:53 -0400

    Refactor Task 1.1-1.2

--- part1/src/App.js
+++ part1/src/App.js
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} exercise1={exercises1} exercise2={exercises2} exercise3={exercises3} />
+      <Content parts={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
--- part1/src/components/Content.js
+++ part1/src/components/Content.js
@@ -1,10 +1,12 @@
 import Part from './Part'
 const Content = (props) => {
+    const parts=[...props.parts]
+    const exercises = [...props.exercises]
     return (
         <div>
-            <Part part={props.part1} excercise={props.excercise1} />
-            <Part part={props.part2} excercise={props.excercise2} />
-            <Part part={props.part3} excercise={props.excercise3} />
+            <Part part={parts[0]} excercise={exercises[0]} />
+            <Part part={parts[1]} excercise={exercises[1]} />
+            <Part part={parts[2]} excercise={exercises[2]} />
         </div>
     )
 }
