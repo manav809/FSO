dd09e006004daeb35506e7ae66ba6e96fdc6b791
manav809 <manavpatel6978@gmail.com>
2023-11-04 21:02:38 -0400

    Add Task 1.4

--- part1/src/components/Content.js
+++ part1/src/components/Content.js
@@ -1,11 +1,10 @@
 import Part from './Part'
 const Content = (props) => {
-    const parts=[...props.parts]
     return (
         <div>
-            <Part part={parts[0].name} excercise={parts[0].exercises} />
-            <Part part={parts[1].name} excercise={parts[1].exercises} />
-            <Part part={parts[2].name} excercise={parts[2].exercises} />
+            <Part part={props.parts[0].name} excercise={props.parts[0].exercises} />
+            <Part part={props.parts[1].name} excercise={props.parts[1].exercises} />
+            <Part part={props.parts[2].name} excercise={props.parts[2].exercises} />
         </div>
     )
 }
