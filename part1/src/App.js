819139fc68d1fea47239399206aabeb96ef8e14d
manav809 <manavpatel6978@gmail.com>
2023-11-05 20:40:49 -0500

    Add Task 1.8

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,29 +1,24 @@
 import Header from './components/Header'
-import Content from './components/Content'
-import Total from './components/Total'
 import { useState } from 'react'
+
+const Statistics = ({good, bad, neutral}) => {
+  return (
+    <>
+      <Header text={'statistics'} />
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good + bad + neutral}</p>
+      <p>average {(good + bad + neutral)/3}</p>
+      <p>positive {(good/(good + bad + neutral)) * 100} %</p>
+    </>
+  )
+}
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      }, 
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of Component', 
-        exercises: 14
-      }
-    ]
-  }
 
   return (
     <div>
@@ -31,13 +26,8 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <Header text={"statistics"}/>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + bad + neutral}</p>
-      <p>average {(good + bad + neutral)/3}</p>
-      <p>positive {(good/(good + bad + neutral)) * 100}</p>
+      <Statistics good={good} bad={bad} neutral={neutral}/>
+
     </div>
   )
 }
