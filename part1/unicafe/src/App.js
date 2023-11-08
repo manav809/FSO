1c238715db4cd7c2d8724c9e78a3987af0121920
manav809 <manavpatel6978@gmail.com>
2023-11-07 20:07:29 -0500

    Refactor File Structure Tasks 1.6-1.11

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,45 @@
+import Header from './components/Header'
+import { useState } from 'react'
+import StatisticLine from './components/StatisticLine'
+import Button from './components/Button'
+const Statistics = ({good, bad, neutral}) => {
+  if(good + bad + neutral === 0){
+    return (
+      <>
+        <Header text={'statistics'} />
+        <h3>no feedback</h3>
+      </>
+    )
+  }
+  return (
+    <>
+      <Header text={'statistics'} />
+      <table>
+        <tbody>
+          <tr><StatisticLine text={"good"} value={good} /></tr>
+          <tr><StatisticLine text={"neutral"} value={neutral} /></tr>
+          <tr><StatisticLine text={"bad"} value={bad} /></tr>
+          <tr><StatisticLine text={"all"} value={good + bad + neutral} /></tr>
+          <tr><StatisticLine text={"average"} value={(good + bad + neutral)/3} /></tr>
+          <tr><StatisticLine text={"positive"} value={(good/(good + bad + neutral)) * 100} /></tr>       
+        </tbody>
+      </table>
+
+    </>
+  )
+}
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  return (
+    <div>
+      <Header text={"give feedback"} />
+      <Button handleClick={() => setGood(good + 1)} text='good' />
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      <Statistics good={good} bad={bad} neutral={neutral}/>
+    </div>
+  )
+}
+export default App
---
+++ part1/unicafe/src/components/StatisticLine.js
@@ -0,0 +1,9 @@
+const StatisticLine = ({text, value}) => {
+    return (
+        <>
+            <td>{text}</td>
+            <td>{value}</td>
+        </>
+    )
+}
+export default StatisticLine;
