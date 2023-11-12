2623cc21c6794ccdb624c3274f046f2871194047
manav809 <manavpatel6978@gmail.com>
2023-11-12 13:23:48 -0500

    Learn Reduce

--- personal_docs/functional programming/filter.js
+++ personal_docs/functional programming/filter.js
@@ -1,36 +1,36 @@
 //functional programming
-var triple = function(x){
-        return x * 3
-}
-var waffle = triple
-waffle(30)
+var triple = function (x) {
+  return x * 3;
+};
+var waffle = triple;
+waffle(30);
 
 //Filter Function Tutorial
 var animals = [
-    {name: 'Fluffykins', species: 'rabbit'}, 
-    {name: 'Caro', species: 'dog'},
-    {name: 'Hamilton', species: 'dog'}, 
-    {name: 'Harold', species: 'fish'}, 
-    {name: 'Ursula', species: 'cat'}, 
-    {name: 'Jimmy', species: 'fish'}
-]
+  { name: "Fluffykins", species: "rabbit" },
+  { name: "Caro", species: "dog" },
+  { name: "Hamilton", species: "dog" },
+  { name: "Harold", species: "fish" },
+  { name: "Ursula", species: "cat" },
+  { name: "Jimmy", species: "fish" },
+];
 //filtering without filter function
-var dogs = []
-for(var i = 0; i < animals.length; i++){
-    if(animals[i].species === 'dog'){
-        dogs.push(animals[i])
-    }
+var dogs = [];
+for (var i = 0; i < animals.length; i++) {
+  if (animals[i].species === "dog") {
+    dogs.push(animals[i]);
+  }
 }
 //with filter function
-var dogs = animals.filter(function(animal){
-    return animal.species === 'dog'
-})
+var dogs = animals.filter(function (animal) {
+  return animal.species === "dog";
+});
 //filter runs a loop and performs the inner function logic and arrifies as we move on
 
-var isDog = function(animal){
-    return animal.species === 'dog'
-}
+var isDog = function (animal) {
+  return animal.species === "dog";
+};
 
-var dogs = animals.filter(isDog);//ARRAY WITH DOGS
-console.log(dogs)
+var dogs = animals.filter(isDog); //ARRAY WITH DOGS
+console.log(dogs);
 // var otherAnimals = animals.reject(isDog);//ARRAY WITHOUT DOGS
--- personal_docs/functional programming/map.js
+++ personal_docs/functional programming/map.js
@@ -1,26 +1,26 @@
 var animals = [
-    {name: 'Fluffykins', species: 'rabbit'}, 
-    {name: 'Caro', species: 'dog'},
-    {name: 'Hamilton', species: 'dog'}, 
-    {name: 'Harold', species: 'fish'}, 
-    {name: 'Ursula', species: 'cat'}, 
-    {name: 'Jimmy', species: 'fish'}
-]
+  { name: "Fluffykins", species: "rabbit" },
+  { name: "Caro", species: "dog" },
+  { name: "Hamilton", species: "dog" },
+  { name: "Harold", species: "fish" },
+  { name: "Ursula", species: "cat" },
+  { name: "Jimmy", species: "fish" },
+];
 
-var names = []
-for(var i = 0; i< animals.length; i++){
-    names.push(animals[i].name)
+var names = [];
+for (var i = 0; i < animals.length; i++) {
+  names.push(animals[i].name);
 }
 
-var names = animals.map(function(animal){
-    return animal.name
-})
+var names = animals.map(function (animal) {
+  return animal.name;
+});
 
-var NAMES = animals.map((animal) => animal.name)
-console.log(NAMES)
+var NAMES = animals.map((animal) => animal.name);
+console.log(NAMES);
 
-var animal_bio = animals.map(function(animal){
-    return animal.name + ' is a ' + animal.species
-})
+var animal_bio = animals.map(function (animal) {
+  return animal.name + " is a " + animal.species;
+});
 
-console.log(animal_bio)
+console.log(animal_bio);
