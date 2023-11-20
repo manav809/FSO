80e59ad45d2c0d5fa9c80ae906ad2a06d16f8554
manav809 <manavpatel6978@gmail.com>
2023-11-20 18:54:10 -0500

    Add UseReducer Tutorial

--- personal_docs/hooks/src/components/useReducer.js
+++ personal_docs/hooks/src/components/useReducer.js
@@ -1,8 +1,20 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + 1, showText: state.showText };
+    case "TOGGLE":
+      return { count: state.count, showText: !state.showText };
+    default:
+      return state;
+  }
+};
 const ReducerTutorial = () => {
-  const [count, setCount] = useState(0);
-  const [showText, setShowText] = useState(true);
+  const [state, dispatch] = useReducer(reducer, {
+    count: 0,
+    showText: true,
+  });
 
   return (
     <div
@@ -12,16 +24,16 @@ const ReducerTutorial = () => {
       }}
     >
       <div style={{ flexDirection: "column" }}>
-        <h1>{count}</h1>
+        <h1>{state.count}</h1>
         <button
           onClick={() => {
-            setCount(count + 1);
-            setShowText(!showText);
+            dispatch({type: "INCREMENT"})
+            dispatch({type: "TOGGLE"})
           }}
         >
           Click Here
         </button>
-        {showText && <p>This is a text</p>}
+        {state.showText && <p>This is a text</p>}
       </div>
     </div>
   );
