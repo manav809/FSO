8e1fc3f1df3020252ac1cecab85a9268f997b562
manav809 <manavpatel6978@gmail.com>
2023-11-19 22:06:15 -0500

    Add UseState Tutorial

--- personal_docs/hooks/src/components/useState.js
+++ personal_docs/hooks/src/components/useState.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const StateTutorial = () => {
-    return <div>Hello</div>
+  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState(0);
+
+  const increment = () => {
+    const count = counter + 1;
+    setCounter(count);
+  };
+  const assign = (event) => {
+    const value = event.target.value
+    setInputValue(value);
+  }
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignContent: "center",
+      }}
+    >
+      {counter} <button onClick={increment}>Increment</button>
+
+      <input placeholder='enter something' onChange={assign}/>{inputValue}
+    </div>
+  );
 };
 
-export default StateTutorial
+export default StateTutorial;
