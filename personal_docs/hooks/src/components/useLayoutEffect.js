5df7ae7ca74fd15c79369ef795ba582d0686e6b6
manav809 <manavpatel6978@gmail.com>
2023-11-21 18:32:09 -0500

    Add UseLayoutEffect Tutorial

--- personal_docs/hooks/src/components/useLayoutEffect.js
+++ personal_docs/hooks/src/components/useLayoutEffect.js
@@ -1,7 +1,22 @@
-import React from 'react'
-
+import React, { useEffect, useLayoutEffect, useRef } from "react";
+//useLayoutEffect is called while loading
+//useEffect is called after everything is loaded
 const LayoutEffectTutorial = () => {
-    return <div>Hello</div>
+  const inputRef = useRef(null);
+  useLayoutEffect(() => {
+    console.log(inputRef.current.value);
+  }, []);
+
+  useEffect(() => {
+    console.log("Hey");
+    inputRef.current.value = "after";
+  }, []);
+
+  return (
+    <div>
+      <input ref={inputRef} value="before" style={{height: "100px"}}/>
+    </div>
+  );
 };
 
-export default LayoutEffectTutorial
+export default LayoutEffectTutorial;
