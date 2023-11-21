e45e491cb88f9b4f6fa36fd6a3aba00baea3a5db
manav809 <manavpatel6978@gmail.com>
2023-11-20 20:49:45 -0500

    Add UseEffect Tutorial

--- personal_docs/hooks/src/components/useEffect.js
+++ personal_docs/hooks/src/components/useEffect.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const EffectTutorial = () => {
-    return <div>Hello</div>
+  const [data, setData] = useState("");
+  const [count, setCount] = useState(0);
+  const [id, setId] = useState(1);
+  useEffect(() => {
+    axios
+      .get(`https://anapioficeandfire.com/api/characters/${id}`)
+      .then((response) => {
+        setId(id => id + 1);
+        console.log(response);
+        setData(response.data.name);
+      });
+  // eslint-disable-next-line
+  }, [count]);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div style={{ flexDirection: "column" }}>
+      <h1>API Response: {data}</h1>
+      {count}
+      <button onClick={increment}>click</button>
+    </div>
+  );
 };
 
-export default EffectTutorial
+export default EffectTutorial;
