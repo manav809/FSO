9cb10dfc9dd4747db88ad8b6eacb2bda65fdae6b
manav809 <manavpatel6978@gmail.com>
2023-11-26 08:01:00 -0500

    Add Task 2.4

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -2,14 +2,20 @@ import Header from "./Header";
 import Content from "./Content";
 import Total from "./Total";
 
-const Course = ({course}) => {
+const Course = ({ courses }) => {
   return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
+    <>
+      {courses.map((course) => {
+        return (
+          <div>
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
+          </div>
+        );
+      })}
+    </>
   );
 };
 
-export default Course
+export default Course;
