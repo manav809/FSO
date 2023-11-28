04bb7a61c6122cc0fcabd48413f39af330b49fdc
manav809 <manavpatel6978@gmail.com>
2023-11-27 21:25:18 -0500

    Add Task 2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,26 +1,35 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
-
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+  const handleChange = (event) => {
+    setNewName(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const person = { name: newName };
+    setPersons(persons.concat(person));
+  };
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map((person) => {
+          return <li key={person.name} >{person.name}</li>;
+        })}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
