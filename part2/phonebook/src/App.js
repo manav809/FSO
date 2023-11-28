49d5399202b17f826d897ed90840f70e271af6c9
manav809 <manavpatel6978@gmail.com>
2023-11-27 21:42:18 -0500

    Add Task 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,13 +3,21 @@ import { useState } from "react";
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+  const [nameAlert, setNameAlert] = useState(true);
   const handleChange = (event) => {
     setNewName(event.target.value);
   };
   const handleSubmit = (event) => {
     event.preventDefault();
     const person = { name: newName };
-    setPersons(persons.concat(person));
+    for (let i = 0; i < persons.length; i++) {
+      if (persons[i].name === newName) {
+        alert(`${newName} is already added to phonebook`);
+      }
+      if (i === persons.length - 1 && persons[i].name != newName) {
+        setPersons(persons.concat(person));
+      }
+    }
   };
   return (
     <div>
@@ -25,7 +33,7 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map((person) => {
-          return <li key={person.name} >{person.name}</li>;
+          return <li key={person.name}>{person.name}</li>;
         })}
       </ul>
     </div>
