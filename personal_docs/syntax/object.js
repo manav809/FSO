acf0b2ac32f40dd2cfe697c10072a29bff7e76e1
manav809 <manavpatel6978@gmail.com>
2023-11-29 21:59:45 -0500

    Objects

---
+++ personal_docs/syntax/object.js
@@ -0,0 +1,13 @@
+
+const person = {
+  name: "Manav",
+  age: 21,
+  isMarried: false,
+};
+
+const { name, age, isMarried } = person;
+
+const person2 = {...person, name="MP"}
+
+const names = ["k", "j", "m"]
+const names2 = [...names, "l"]
