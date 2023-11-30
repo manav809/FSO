a72e47daf80a90c9f2bf7201de7473a520b4e62c
manav809 <manavpatel6978@gmail.com>
2023-11-29 21:27:36 -0500

    Arrow Functions

---
+++ personal_docs/syntax/arrow.js
@@ -0,0 +1,13 @@
+export default function DoSomething() {}
+
+export const DoSomething2 = () => {};
+
+const MyComponent = () => {
+  return <div></div>;
+};
+
+<button
+  onClick={() => {
+    console.log("Hey");
+  }}
+></button>;
