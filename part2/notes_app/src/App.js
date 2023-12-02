b5093e4444956086a1897e1950c5249a5a1c6e94
manav809 <manavpatel6978@gmail.com>
2023-12-02 11:22:04 -0500

    Axios Summary

--- part2/notes_app/src/App.js
+++ part2/notes_app/src/App.js
@@ -1,10 +1,24 @@
 import Note from "./components/Note";
-import { useState } from "react";
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const App = () => {
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
-  const notesToShow = showAll ? notes : notes.filter((note) => note.important === true)
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true);
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log("promise fulfilled");
+      setNotes(response.data);
+    });
+  }, []);
+
+  console.log("render", notes.length, "notes");
   const addNote = (event) => {
     event.preventDefault();
     console.log("button clicked", event.target);
@@ -16,15 +30,17 @@ const App = (props) => {
     setNotes(notes.concat(noteObject));
     setNewNote("");
   };
+
   const handleNoteChange = (event) => {
     setNewNote(event.target.value);
   };
+
   return (
     <div>
       <h1>Notes</h1>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all'}
+          show {showAll ? "important" : "all"}
         </button>
       </div>
       <ul>
@@ -41,3 +57,32 @@ const App = (props) => {
 };
 
 export default App;
+
+/* Alternative way to define useeffect
+const hook = () => {
+  console.log('effect')
+  axios
+    .get('http://localhost:3001/notes')
+    .then(response => {
+      console.log('promise fulfilled')
+      setNotes(response.data)
+    })
+}
+
+useEffect(hook, [])
+*/
+
+/* Alternative way to do event handling
+
+useEffect(() => {
+  console.log('effect')
+
+  const eventHandler = response => {
+    console.log('promise fulfilled')
+    setNotes(response.data)
+  }
+
+  const promise = axios.get('http://localhost:3001/notes')
+  promise.then(eventHandler)
+}, [])
+*/
