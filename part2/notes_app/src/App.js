2aa2c66b55ab2ffea84ab5098506221e8e314e37
manav809 <manavpatel6978@gmail.com>
2023-12-02 22:49:56 -0500

    HTTP PUT Axios: Updating Data

--- part2/notes_app/src/App.js
+++ part2/notes_app/src/App.js
@@ -6,6 +6,19 @@ const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
+
+  const toggleImportanceOf = (id) => {
+    console.log("importance of " + id + " needs to be toggled");
+
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notes.find((note) => note.id === id);
+    const changedNote = { ...note, important: !note.important };
+    axios.put(url, changedNote).then((response) => {
+      setNotes(notes.map((n) => (n.id != id ? n : response.data)));
+    });
+
+  };
+
   const notesToShow = showAll
     ? notes
     : notes.filter((note) => note.important === true);
@@ -47,7 +60,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
