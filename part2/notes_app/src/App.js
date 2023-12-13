ee3f7b90e7dd85b507ef55574f6cb7809996c2c3
manav809 <manavpatel6978@gmail.com>
2023-12-12 22:30:50 -0500

    Add JS CSS Variable Inline

--- part2/notes_app/src/App.js
+++ part2/notes_app/src/App.js
@@ -2,6 +2,7 @@ import Note from "./components/Note";
 import { useState, useEffect } from "react";
 import notesService from "./services/notes";
 import Notification from "./components/Notification";
+import Footer from "./components/Footer";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
@@ -82,6 +83,7 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   );
 };
