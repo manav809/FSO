1a847b733451b52ec82f16c29703f8923f959d5c
manav809 <manavpatel6978@gmail.com>
2023-12-12 20:11:04 -0500

    Intro to CSS

--- part2/notes_app/src/components/Note.js
+++ part2/notes_app/src/components/Note.js
@@ -3,8 +3,7 @@ const Note = ({ note, toggleImportance }) => {
   const label = note.important ? "make not important" : "make important";
   return (
     <li>
-      {note.content}
-      <button onClick={toggleImportance}>{label}</button>
+      {note.content} <button onClick={toggleImportance}>{label}</button>
     </li>
   );
 };
