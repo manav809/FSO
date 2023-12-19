dfc0ede63b115b77377a51fdff2ccf181851b54b
manav809 <manavpatel6978@gmail.com>
2023-12-19 17:54:59 -0500

    Integrate Country App

---
+++ part2/exchange_app/src/views/Country.js
@@ -0,0 +1,10 @@
+const Country = () => {
+  const styles = {
+    country: {
+      float: "right",
+    },
+  };
+  return <div style={styles.country}>Country Application</div>;
+};
+
+export default Country;
