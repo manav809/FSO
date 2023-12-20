3626726429291e22cffe46bd69b71ec2d9a9216e
manav809 <manavpatel6978@gmail.com>
2023-12-19 21:11:40 -0500

    Add Task 2.18

--- part2/exchange_app/src/views/Country.js
+++ part2/exchange_app/src/views/Country.js
@@ -3,8 +3,11 @@ import axios from "axios";
 
 const Country = () => {
   const [value, setValue] = useState("");
-  const [countries, setCountries] = useState([]);
-  const [matched, setMatched] = useState();
+  const [countriesNames, setCountriesNames] = useState([]);
+  const [matched, setMatched] = useState(null);
+  const [prediction, setPrediction] = useState([]);
+
+  const [matchedCountry, setMatchedCountry] = useState({});
   const styles = {
     country: {
       float: "left",
@@ -15,18 +18,65 @@ const Country = () => {
     axios
       .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
       .then((response) => {
-        setCountries(response.data);
-        console.log(response.data);
+        setCountriesNames(
+          response.data.map((country) => {
+            return country.name.common;
+          })
+        );
       });
   }, []);
 
+  useEffect(() => {
+    matched
+      ? axios
+          .get(
+            `https://studies.cs.helsinki.fi/restcountries/api/name/${matched}`
+          )
+          .then((response) => {
+            setMatchedCountry(response.data);
+            console.log(response.data);
+          })
+          .catch((err) => console.log(err))
+      : setMatchedCountry({});
+  }, [matched]);
+
   const handleChange = (event) => {
     setValue(event.target.value);
+    let names = countriesNames
+      .map((name) => {
+        if (name.includes(event.target.value)) {
+          return name;
+        } else {
+          return null;
+        }
+      })
+      .filter(Boolean);
+
+    event.target.value !== "" ? setPrediction(names) : setPrediction([]);
+
+    names.length === 1 ? setMatched(names[0]) : setMatched(null);
   };
   return (
     <div style={styles.country}>
       <h1>Country Wiki</h1>
       Find Country: <input value={value} onChange={handleChange} />
+      {prediction.length < 10 && Object.keys(matchedCountry).length === 0 ? (
+        prediction.map((country) => <p>{country}</p>)
+      ) : Object.keys(matchedCountry).length > 0 ? (
+        <div>
+          <h1>{matchedCountry.name.common}</h1>
+          <p>Capital: {matchedCountry.capital[0]}</p>
+          <p>Area Code: {matchedCountry.area}</p>
+          <h3>Languages: </h3>
+          {Object.values(matchedCountry.languages).map((language) => (
+            <li>{language}</li>
+          ))}
+          <h3>Flag</h3>
+          <img src={matchedCountry.flags["png"]} alt="flag" />
+        </div>
+      ) : (
+        <p>Too many matches... please be specific</p>
+      )}
     </div>
   );
 };
