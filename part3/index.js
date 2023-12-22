d58bc014fd2f00ee28f933b9009a61bf0198772b
manav809 <manavpatel6978@gmail.com>
2023-12-21 21:48:57 -0500

    Basic Backend Server

---
+++ part3/index.js
@@ -0,0 +1,14 @@
+console.log("hello world");
+
+const http = require("http");
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello World");
+});
+
+const PORT = 3001;
+
+app.listen(PORT);
+
+console.log(`Server is running on port ${PORT}`);
