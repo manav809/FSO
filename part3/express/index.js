7ee5f3895de90478c9299df6015c035ad647d88b
manav809 <manavpatel6978@gmail.com>
2023-12-22 12:52:03 -0500

    GET and DELETE

--- part3/express/index.js
+++ part3/express/index.js
@@ -1,36 +1,50 @@
-const express = require('express')
-const app = express()
-const listEndpoints = require('express-list-endpoints')
+const express = require("express");
+const app = express();
+const listEndpoints = require("express-list-endpoints");
 
 let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true,
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false,
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true,
-    },
-  ];
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World</h1>')
-})
-  
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const note = notes.find((note) => {
+    return note.id === id;
+  });
+  note ? res.json(note) : res.status(404).end();
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  notes = notes.filter((note) => note.id !== id);
+  res.send(204).end();
+});
 
 const PORT = 3001;
 
 app.listen(PORT);
 
-console.log(`Server is running on port ${PORT}`)
-console.table(listEndpoints(app))
+console.log(`Server is running on port ${PORT}`);
+console.table(listEndpoints(app));
