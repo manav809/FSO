a42d3f9dc8b15018bcaa51a1626abf7473b120e2
manav809 <manavpatel6978@gmail.com>
2023-12-22 10:21:42 -0500

    Add Notes JSON

--- part3/index.js
+++ part3/index.js
@@ -2,9 +2,27 @@ console.log("hello world");
 
 const http = require("http");
 
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World");
+  res.writeHead(200, { "Content-Type": "application/json" }); // application/json informs the reciever that the data is in JSON format instead of text/plain
+  res.end(JSON.stringify(notes));
 });
 
 const PORT = 3001;
