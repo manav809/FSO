c547a38d9972839f9245bbbaeca88fbc86e82af4
manav809 <manavpatel6978@gmail.com>
2024-01-04 20:05:42 -0500

    Add Task 3.2

--- part3/phonebook-server/index.js
+++ part3/phonebook-server/index.js
@@ -28,6 +28,10 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello World</h1>");
 });
 
+app.get("/info", (req, res) => {
+  res.send(`Phonebook has info for ${persons.length} people <br /> ${Date()}`);
+});
+
 app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
