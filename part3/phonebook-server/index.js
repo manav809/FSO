20b6bdf9916b2bcc9257676259ae86bd041fc5a2
manav809 <manavpatel6978@gmail.com>
2024-01-04 21:42:05 -0500

    Add Task 3.4

--- part3/phonebook-server/index.js
+++ part3/phonebook-server/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const listEndpoints = require("express-list-endpoints");
 const app = express();
 
-const persons = [
+let persons = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -42,7 +43,17 @@ app.get("/api/persons/:id", (req, res) => {
   person ? res.json(person) : res.status(404).end();
 });
 
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  console.log(persons)
+  res.send(204).end();
+});
+
 const PORT = 3001;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+console.table(listEndpoints(app));
