47d61dbd1d830efd7f9a2f135359c8ea559fe729
manav809 <manavpatel6978@gmail.com>
2024-01-04 21:27:36 -0500

    Add Task 3.3

--- part3/phonebook-server/index.js
+++ part3/phonebook-server/index.js
@@ -36,6 +36,12 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+  person ? res.json(person) : res.status(404).end();
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
