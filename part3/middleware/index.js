3da4b7cadb462071ea844e6b81049402080726cc
manav809 <manavpatel6978@gmail.com>
2024-01-07 15:28:05 -0500

    Add Middleware Example

--- part3/middleware/index.js
+++ part3/middleware/index.js
@@ -1,12 +1,33 @@
 const express = require("express");
+const listEndpoints = require("express-list-endpoints");
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // this is a middleware that we use without realizing: when recieving the HTTP request, parse it like a json
+
+const checkIfManav = (req, res, next) => {
+  const name = req.body.name;
+  if (name === "Manav") {
+    res.json({ error: "Yo we already have you on db" });
+  } else {
+    next();
+  }
+};
+
+app.use((req, res, next) => {
+  console.log("Middleware Called");
+  next();
+});
 
 app.get("/", (req, res) => {
-  res.join({ message: "Hello World" });
+  res.json({ message: "Hello World" });
+});
+
+app.post("/", checkIfManav, (req, res) => {
+  res.send("You logged in");
 });
 
 app.listen(3001, () => {
-    console.log("Server running");
-})
+  console.log("Server running");
+});
+
+console.table(listEndpoints(app));
