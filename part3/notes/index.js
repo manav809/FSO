85cbafd745dce38bed3ba6b28b4b3ab9e907a0b5
manav809 <manavpatel6978@gmail.com>
2024-02-08 20:50:02 -0500

    Mongoosify Notes App

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -7,23 +7,6 @@ const app = express();
 
 app.use(cors());
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
 const requestLogger = (req, res, next) => {
   console.log("Method: ", req.method);
   console.log("Path: ", req.path);
@@ -51,48 +34,40 @@ app.get("/api/notes", (req, res) => {
 
 //Get all Notes by Id
 app.get("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const note = notes.find((note) => {
-    return note.id === id;
-  });
-  note ? res.json(note) : res.status(404).end();
+  const id = req.params.id;
+  Notes.findById(id)
+    .then((notes) => {
+      res.json(notes);
+    })
+    .catch((error) => res.sendStatus(404));
 });
 
 //Delete a Note by ID
 app.delete("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  notes = notes.filter((note) => note.id !== id);
-  res.send(204).end();
+  const id = req.params.id;
+  Notes.deleteOne({ _id: id }).then(() => res.sendStatus(204));
 });
 
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
 //Add a Note
 app.post("/api/notes", (req, res) => {
   const body = req.body;
+
   if (!body.content) {
     return res.status(400).json({ error: "Missing Content" });
   }
-  const note = {
-    id: generateId(),
+  const note = new Notes({
     content: body.content,
     important: Boolean(body.important) || false,
-  };
-
-  notes = notes.concat(note);
-
-  res.json(note);
+  });
+  note.save().then((saved) => {
+    res.json(saved);
+  });
 });
+
 app.put("/api/notes/:id", (req, res) => {
   const body = req.body;
-  notes.map((note) => {
-    if (note.id === body.id) {
-      note.important === body.important;
-    }
-  });
-  res.json(body);
+  const id = req.params.id;
+  Notes.findByIdAndUpdate({ _id: id }, body).then((body) => res.json(body));
 });
 const PORT = 3001;
 
