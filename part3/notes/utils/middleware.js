03bf8426e0f56287d7bf0a2c5ec43386b6e00311
manav809 <manavpatel6978@gmail.com>
2024-03-12 19:34:10 -0400

    Add Middleware Util

---
+++ part3/notes/utils/middleware.js
@@ -0,0 +1,23 @@
+const logger = require("./logger");
+
+const requestLogger = (req, res, next) => {
+  logger.info("Method: ", req.method);
+  logger.info("Path: ", req.path);
+  logger.info("Body: ", req.body);
+  logger.info("---");
+  next();
+};
+
+const errorHandler = (error, req, res, next) => {
+  logger.error(error.message);
+  if (error.name === "CastError") {
+    return res.status(404).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return res.status(400).json({ error: error.message });
+  }
+  next(error);
+};
+
+module.exports = {
+  requestLogger, errorHandler
+}
