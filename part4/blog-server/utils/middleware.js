629f79f55a5b0d5a2d0ec9ae654e6327417aef00
manav809 <manavpatel6978@gmail.com>
2024-03-19 22:44:02 -0400

    Add Blog Server Utils

---
+++ part4/blog-server/utils/middleware.js
@@ -0,0 +1,28 @@
+const logger = require("./logger");
+
+const requestLogger = (req, res, next) => {
+  logger.info("Method: ", req.method);
+  logger.info("Path", req.path);
+  logger.info("Body: ", req.body);
+  logger.info("------");
+  next();
+};
+
+const erroHandler = (error, req, res, next) => {
+  logger.error(error);
+  if (error.name === "CastError") {
+    return res.status(404).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return res.status(400).json({ error: error.message });
+  } else {
+    return res
+      .status(400)
+      .json({ error: error.message, devMessage: "Set Breakpoint" });
+  }
+  next(error);
+};
+
+module.exports = {
+  requestLogger,
+  erroHandler,
+};
