84d8f00faf50c3940a8e3189e08ece6a8380289b
manav809 <manavpatel6978@gmail.com>
2024-03-19 22:02:46 -0400

    Add listEndpoints for Blog Server

--- part4/blog-server/index.js
+++ part4/blog-server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+const listEndpoints = require("express-list-endpoints");
 require("dotenv").config();
 
 const blogSchema = new mongoose.Schema({
@@ -15,7 +16,7 @@ const Blog = mongoose.model("Blog", blogSchema);
 
 const mongoUrl = process.env.MONGO_URL;
 
-mongoose.set('strictQuery', false)
+mongoose.set("strictQuery", false);
 
 mongoose.connect(mongoUrl);
 
@@ -23,13 +24,21 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/api/blogs", (req, res) => {
+  console.log(req);
   Blog.find({}).then((result) => {
     res.status(201).json(result);
   });
 });
 
-app.post("api/blogs", (req, res) => {
-  const blog = new Blog(req.body);
+app.post("/api/blogs", async (req, res) => {
+  console.log("Hey")
+  const body = req.body;
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  });
   blog.save().then((result) => {
     res.status(201).json(result);
   });
@@ -39,3 +48,5 @@ const PORT = 3003;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+console.table(listEndpoints(app))
