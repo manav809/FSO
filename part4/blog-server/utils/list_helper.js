54736a12d22d698f23c5aa45abaa884767e4bf2a
manav809 <manavpatel6978@gmail.com>
2024-03-22 13:59:15 -0400

    Add Task 4.5

--- part4/blog-server/utils/list_helper.js
+++ part4/blog-server/utils/list_helper.js
@@ -8,7 +8,23 @@ const totalLikes = (blogs) => {
   };
   return blogs.reduce(reducer, 0);
 };
+
+const highestLikes = (blogs) => {
+  // First, get the max vote from the array of objects
+  var maxVotes = Math.max(...blogs.map((e) => e.likes));
+
+  // Get the object having votes as max votes
+  var blog = blogs.find((blog) => blog.likes === maxVotes);
+
+  delete blog._id;
+  delete blog.url;
+  delete blog.__v;
+  console.log(blog);
+  return blog;
+};
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  highestLikes,
 };
