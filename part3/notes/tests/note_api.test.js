64c17a293049d158d1c96ab4eab1901dff9f4f2b
manav809 <manavpatel6978@gmail.com>
2024-03-27 21:56:44 -0400

    Add Before Each

--- part3/notes/tests/note_api.test.js
+++ part3/notes/tests/note_api.test.js
@@ -1,10 +1,30 @@
-const { test, after } = require("node:test");
+const { test, after, beforeEach } = require("node:test");
 const assert = require("node:assert");
+const Note = require("../models/note");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
 
 const api = supertest(app);
+
+const initialNotes = [
+  {
+    content: "HTML is easy",
+    important: false,
+  },
+  {
+    content: "Browser executes JavaScript",
+    important: true,
+  },
+];
+
+beforeEach(async () => {
+  await Note.deleteMany({});
+  let noteObj = new Note(initialNotes[0]);
+  await noteObj.save();
+  noteObj = new Note(initialNotes[1]);
+  await noteObj.save();
+});
 
 test("notes are returned as json", async () => {
   await api
@@ -15,14 +35,13 @@ test("notes are returned as json", async () => {
 
 test("there is 1 note", async () => {
   const res = await api.get("/api/notes");
-  assert.strictEqual(res.body.length, 1);
+  assert.strictEqual(res.body.length, initialNotes.length);
 });
 
-test("the note is s greeting", async () => {
+test("the note first note tells us that HTML is easy", async () => {
   const res = await api.get("/api/notes");
-
-  assert.strictEqual(res.body[0].content, "Hey");
-  
+  const contents = res.body.map(obj => obj.content);
+  assert.strictEqual(contents[0], "HTML is easy");
 });
 
 after(async () => {
