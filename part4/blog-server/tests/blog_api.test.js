fb038804ec1ce6dc13ba082eb5158d3d1879de9f
manav809 <manavpatel6978@gmail.com>
2024-04-01 19:57:55 -0400

    Add Task 4.9

--- part4/blog-server/tests/blog_api.test.js
+++ part4/blog-server/tests/blog_api.test.js
@@ -1,10 +1,10 @@
-const { test, after, beforeEach } = require("node:test");
+const { test, after, beforeEach, expect } = require("node:test");
+const assert = require("node:assert");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
 const Blogs = require("../models/blog");
 const helper = require("./test_helper");
-const assert = require("node:assert");
 
 const api = supertest(app);
 
@@ -24,6 +24,16 @@ test("blogs are returned as json", async () => {
 
   const response = await helper.blogsInDb();
   assert.strictEqual(helper.initialBlogs.length, response.length);
+});
+
+test("blogs have id attribute", async () => {
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+
+  const response = await helper.blogsInDb();
+  assert('id' in response[0])
 });
 
 after(async () => {
