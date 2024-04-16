c4cbe1a5bfb6e9f1574a4585f612d4ea3b0cdd49
manav809 <manavpatel6978@gmail.com>
2024-04-15 21:03:53 -0400

    Add User Route

--- part3/notes/app.js
+++ part3/notes/app.js
@@ -6,6 +6,7 @@ require("express-async-errors"); // we dont need try and catch now
 const cors = require("cors");
 const mongoose = require("mongoose");
 const notesRouter = require("./controllers/notes");
+const usersRouter = require("./controllers/users");
 require("dotenv").config();
 
 const app = express();
@@ -27,6 +28,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use("/api/notes", notesRouter);
+app.use("/api/users", usersRouter);
 
 app.use(middleware.errorHandler);
 
