22ff286eadcd006ef072aa5163d2bacc6bd7f802
manav809 <manavpatel6978@gmail.com>
2024-04-17 19:37:06 -0400

    Add Login Router

--- part3/notes/app.js
+++ part3/notes/app.js
@@ -7,6 +7,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const notesRouter = require("./controllers/notes");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 require("dotenv").config();
 
 const app = express();
@@ -29,6 +30,7 @@ app.use(middleware.requestLogger);
 
 app.use("/api/notes", notesRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.errorHandler);
 
