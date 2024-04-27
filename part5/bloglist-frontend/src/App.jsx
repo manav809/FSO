fd9048618c093de16dfa775428bccdebd732e3f5
manav809 <manavpatel6978@gmail.com>
2024-04-26 22:01:21 -0400

    Add Task 5.1

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,24 +1,64 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      console.log("Some Error");
+    }
+  };
 
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+      {!user ? <p>Please Log In</p> : <p>{user.name} Logged In</p>}
+      {!user ? (
+        <form onSubmit={handleLogin}>
+          <div>
+            username{" "}
+            <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={(event) => setUsername(event.target.value)}
+            />
+          </div>
+          <div>
+            password{" "}
+            <input
+              type="text"
+              value={password}
+              name="Password"
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </div>
+          <button type="submit">login</button>
+        </form>
+      ) : (
+        blogs.map((blog) => <Blog key={blog.id} blog={blog} />)
       )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
