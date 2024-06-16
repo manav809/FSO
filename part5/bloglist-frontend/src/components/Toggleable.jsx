91ca92175bf1ef3e2e4c280a1e21e35d83b9f6e9
manav809 <manavpatel6978@gmail.com>
2024-06-16 15:53:49 -0400

    Add Task 5.5

---
+++ part5/bloglist-frontend/src/components/Toggleable.jsx
@@ -0,0 +1,32 @@
+import { useState, useImperativeHandle, forwardRef } from "react";
+
+const Toggleable = forwardRef((props, refs) => {
+  const [visible, setVisible] = useState(false);
+
+  const hideWhenVisible = { display: visible ? "none" : "" };
+  const showWhenVisible = { display: visible ? "" : "none" };
+
+  const toggleVisiblity = () => {
+    setVisible(!visible);
+  };
+
+  useImperativeHandle(refs, () => {
+    return {
+      toggleVisiblity,
+    };
+  });
+  
+  return (
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={toggleVisiblity}>{props.buttonLabel}</button>
+      </div>
+      <div style={showWhenVisible}>
+        {props.children}
+        <button onClick={toggleVisiblity}>cancel</button>
+      </div>
+    </div>
+  );
+});
+
+export default Toggleable;
