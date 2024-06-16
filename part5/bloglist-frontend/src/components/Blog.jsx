4855e762920cc3dc89cfc6472803405ca99f6bbf
manav809 <manavpatel6978@gmail.com>
2024-06-16 16:58:15 -0400

    Add Task 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author?.name}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  const [expand, setExpand] = useState(false);
+
+  const expandMore = (event) => {
+    event.preventDefault();
+    setExpand(!expand);
+  };
+  
+  const label = expand ? "hide" : "view"
+  return (
+    <div style={blogStyle}>
+      {blog.title} {" "}
+      <button onClick={expandMore}>{label}</button>
+      {expand ? (
+        <>
+          <p>{blog.url}</p>
+          <p>likes {blog.likes} <button>likes</button></p>
+          <p>{blog.author?.name} </p>
+        </>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
