5e0d1bfdc92092b6ff569a23d32ccd84a687efd6
manav809 <manavpatel6978@gmail.com>
2024-06-18 18:15:41 -0400

    Add Vitest For Note

---
+++ part2/notes_app/src/components/Note.test.jsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react'
+import Note from './Note'
+
+test('renders content', () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true
+  }
+
+  render(<Note note={note} />)
+
+})
