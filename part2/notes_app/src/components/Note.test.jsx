0046333f51540d2b5b9d9257290a5975c22c87c1
manav809 <manavpatel6978@gmail.com>
2024-06-19 15:14:18 -0400

    Add Clicking Button Test

--- part2/notes_app/src/components/Note.test.jsx
+++ part2/notes_app/src/components/Note.test.jsx
@@ -1,28 +1,48 @@
-import { render, screen } from '@testing-library/react'
-import Note from './Note'
+import { render, screen } from "@testing-library/react";
+import Note from "./Note";
+import userEvent from "@testing-library/user-event";
 
-test('renders content', () => {
+test("renders content", () => {
   const note = {
-    content: 'Component testing is done with react-testing-library',
-    important: true
-  }
+    content: "Component testing is done with react-testing-library",
+    important: true,
+  };
 
-  render(<Note note={note} />)
+  render(<Note note={note} />);
 
-  const element = screen.getByText('Component testing is done with react-testing-library')
+  const element = screen.getByText(
+    "Component testing is done with react-testing-library"
+  );
 
-  expect(element).toBeDefined()
-})
+  expect(element).toBeDefined();
+});
 
-test('renders div', () => {
+test("renders div", () => {
   const note = {
-    content: 'Component testing is done with react-testing-library',
-    important: true
-  }
+    content: "Component testing is done with react-testing-library",
+    important: true,
+  };
 
+  const { container } = render(<Note note={note} />);
+  const div = container.querySelector(".note");
 
-  const {container} = render(<Note note={note} />)
-  const div =  container.querySelector('.note')
+  expect(div).toHaveTextContent(
+    "Component testing is done with react-testing-library"
+  );
+});
+
+test("clicking the button calles the event handler once", async () => {
+  const note = {
+    content: "Component testing is done with react-testing-library",
+    important: true,
+  };
+  const mockHandler = vi.fn();
+
+  render(<Note note={note} toggleImportance={mockHandler} />);
+
+  const user = userEvent.setup();
+  const button = screen.getByText("make not important");
+  await user.click(button);
   
-  expect(div).toHaveTextContent('Component testing is done with react-testing-library')
-})
+  expect(mockHandler.mock.calls).toHaveLength(1);
+});
