00f22cceead0d69db47d5fdadfe41a8398e66600
manav809 <manavpatel6978@gmail.com>
2024-06-25 18:38:29 -0400

    Add Test Note Creation

--- part5/notes_e2e/tests/note_app.spec.js
+++ part5/notes_e2e/tests/note_app.spec.js
@@ -22,4 +22,22 @@ describe("Note App", () => {
 
     await expect(page.getByText('Welcome Vidhi, below are your notes')).toBeVisible()
   });
+
+  describe("when logged in", () => {
+    beforeEach(async ({page}) => {
+      await page.getByRole("button", { name: "login" }).click();
+      const textboxes = await page.getByRole('textbox').all()
+      await textboxes[0].fill('vidhi809')
+      await textboxes[1].fill('123')
+      await page.getByRole("button", { name: "login" }).click();
+    })
+    test("note creation", async ({page}) => {
+      await page.getByRole("button", {name: "new note"}).click();
+      const textboxes = await page.getByRole('textbox').all()
+      await textboxes[0].fill('Playwright Testing is Cool')
+      await page.getByRole('button', {name: "save"}).click()
+
+      await expect(page.getByText('Playwright Testing is Cool')).toBeVisible()
+    })
+  })
 });
