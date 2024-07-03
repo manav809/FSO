6bd38cde3bd2061daf033e0da1abf67905f6db82
manav809 <manavpatel6978@gmail.com>
2024-07-03 19:48:05 -0400

    Add Simple Counter App that Uses Redux

--- part6/counter/src/main.jsx
+++ part6/counter/src/main.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createStore } from "redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const countReducer = (state = 0, action) => {
+  switch (action.type){
+    case "INCREMENT":
+      return state + 1
+    case "DECREMENT":
+      return state - 1
+    case "ZERO":
+      return 0
+    default:
+      return state
+  }
+}
+
+
+const store = createStore(countReducer)
+
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        {store.getState()}
+      </div>
+      <div>
+        <button onClick={e => store.dispatch({type: "INCREMENT"})}>
+          plus
+        </button>
+        <button onClick={e => store.dispatch({type: "DECREMENT"})}>
+          minus
+        </button>
+        <button onClick={e => store.dispatch({type: "ZERO"})}>
+          zero
+        </button>
+      </div>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+
+store.subscribe(() => {
+  renderApp()
+  console.log(store.getState())
+})
