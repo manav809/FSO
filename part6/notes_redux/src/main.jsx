eb6d6599fdf0dc53ec7751d469f2aa6a5de61457
manav809 <manavpatel6978@gmail.com>
2024-07-04 13:21:37 -0400

    Add TDD

--- part6/notes_redux/src/main.jsx
+++ part6/notes_redux/src/main.jsx
@@ -1,14 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createStore } from "redux";
-
-const noteReducer = (state = [], action) => {
-  if (action.type === 'NEW_NOTE') {
-    return state.concat(action.payload)
-  }
-
-  return state
-}
+import noteReducer from "./reducers/noteReducer";
 
 const store = createStore(noteReducer)
 
