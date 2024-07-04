4ff7b9e4233b2bb697cffb199b75ccd68d02054a
manav809 <manavpatel6978@gmail.com>
2024-07-04 12:47:04 -0400

    Add Simple Redux Notes App

---
+++ part6/notes_redux/src/main.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createStore } from "redux";
+
+const noteReducer = (state = [], action) => {
+  if (action.type === 'NEW_NOTE') {
+    return state.concat(action.payload)
+  }
+
+  return state
+}
+
+const store = createStore(noteReducer)
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'If there is a change in the state, the old object is not changed, but it is replaced with a new, changed, object.',
+    important: true,
+    id: 1
+  }
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'The general convention is that actions have exactly two fields, type telling the type and payload containing the data included with the Action.',
+    important: false,
+    id: 2
+  }
+})
+
+const App = () => {
+  return(
+    <div>
+      <ul>
+        {store.getState().map(note=>
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        )}
+        </ul>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+
+store.subscribe(() => {
+  renderApp()
+  console.log(store.getState())
+})
