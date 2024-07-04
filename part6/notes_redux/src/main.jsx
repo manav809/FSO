44f1112a7e3a64203f712479987971b191d18d13
manav809 <manavpatel6978@gmail.com>
2024-07-04 14:46:58 -0400

    Add Action Creators

--- part6/notes_redux/src/main.jsx
+++ part6/notes_redux/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createStore } from "redux";
 import noteReducer from "./reducers/noteReducer";
+import  {createNote, toggleImportanceOf} from "./reducers/actions";
 
 const store = createStore(noteReducer);
 
@@ -25,42 +26,31 @@ store.dispatch({
   },
 });
 
-const generateId = () => Number((Math.random() * 1000000).toFixed(0));
-
 const App = () => {
   const addNote = (event) => {
     event.preventDefault();
     const content = event.target.note.value;
     event.target.note.value = "";
 
-    store.dispatch({
-      type: "NEW_NOTE",
-      payload: {
-        content,
-        important: false,
-        id: generateId(),
-      },
-    });
+    store.dispatch(createNote(content));
   };
 
   const toggleImportance = (id) => {
-    store.dispatch({
-      type: "TOGGLE_IMPORTANCE",
-      payload: { id },
-    });
+    store.dispatch(toggleImportanceOf(id));
   };
 
   return (
     <div>
       <form onSubmit={addNote}>
-        <input name='note'/>
+        <input name="note" />
         <button type="submit">add</button>
       </form>
 
       <ul>
         {store.getState().map((note) => (
           <li key={note.id} onClick={() => toggleImportance(note.id)}>
-            {note.content} <strong>{note.important ? "important" : "not important"}</strong>
+            {note.content}{" "}
+            <strong>{note.important ? "important" : "not important"}</strong>
           </li>
         ))}
       </ul>
---
+++ part6/notes_redux/src/reducers/actions.js
@@ -0,0 +1,20 @@
+const generateId = () => Number((Math.random() * 1000000).toFixed(0));
+
+export const createNote = (content) => {
+  return {
+    type: "NEW_NOTE",
+    payload: {
+      content,
+      important: false,
+      id: generateId(),
+    },
+  };
+};
+
+export const toggleImportanceOf = (id) => {
+  return {
+    type: "TOGGLE_IMPORTANCE",
+    payload: { id },
+  };
+};
+
