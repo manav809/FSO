3a73fe93f63f8de73084b00c2697853610429464
manav809 <manavpatel6978@gmail.com>
2024-07-04 14:17:21 -0400

    Add Uncontr olled Notes Form

--- part6/notes_redux/src/main.jsx
+++ part6/notes_redux/src/main.jsx
@@ -3,49 +3,80 @@ import ReactDOM from "react-dom/client";
 import { createStore } from "redux";
 import noteReducer from "./reducers/noteReducer";
 
-const store = createStore(noteReducer)
+const store = createStore(noteReducer);
 
 store.dispatch({
-  type: 'NEW_NOTE',
+  type: "NEW_NOTE",
   payload: {
-    content: 'If there is a change in the state, the old object is not changed, but it is replaced with a new, changed, object.',
+    content:
+      "If there is a change in the state, the old object is not changed, but it is replaced with a new, changed, object.",
     important: true,
-    id: 1
-  }
-})
+    id: 1,
+  },
+});
 
 store.dispatch({
-  type: 'NEW_NOTE',
+  type: "NEW_NOTE",
   payload: {
-    content: 'The general convention is that actions have exactly two fields, type telling the type and payload containing the data included with the Action.',
+    content:
+      "The general convention is that actions have exactly two fields, type telling the type and payload containing the data included with the Action.",
     important: false,
-    id: 2
-  }
-})
+    id: 2,
+  },
+});
+
+const generateId = () => Number((Math.random() * 1000000).toFixed(0));
 
 const App = () => {
-  return(
-    <div>
-      <ul>
-        {store.getState().map(note=>
-          <li key={note.id}>
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        )}
-        </ul>
-    </div>
-  )
-}
+  const addNote = (event) => {
+    event.preventDefault();
+    const content = event.target.note.value;
+    event.target.note.value = "";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+    store.dispatch({
+      type: "NEW_NOTE",
+      payload: {
+        content,
+        important: false,
+        id: generateId(),
+      },
+    });
+  };
+
+  const toggleImportance = (id) => {
+    store.dispatch({
+      type: "TOGGLE_IMPORTANCE",
+      payload: { id },
+    });
+  };
+
+  return (
+    <div>
+      <form onSubmit={addNote}>
+        <input name='note'/>
+        <button type="submit">add</button>
+      </form>
+
+      <ul>
+        {store.getState().map((note) => (
+          <li key={note.id} onClick={() => toggleImportance(note.id)}>
+            {note.content} <strong>{note.important ? "important" : "not important"}</strong>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const renderApp = () => {
-  root.render(<App />)
-}
+  root.render(<App />);
+};
 
-renderApp()
+renderApp();
 
 store.subscribe(() => {
-  renderApp()
-  console.log(store.getState())
-})
+  renderApp();
+  console.log(store.getState());
+});
