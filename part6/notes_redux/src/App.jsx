7a5e8f8dc92ccdc619ee0ab4e1f915aaa2bb681f
manav809 <manavpatel6978@gmail.com>
2024-07-06 13:24:38 -0400

    Forward Redux Store

--- part6/notes_redux/src/App.jsx
+++ part6/notes_redux/src/App.jsx
@@ -1,12 +1,40 @@
-import { useState } from "react";
+import  {createNote, toggleImportanceOf} from "./reducers/actions";
+import { useSelector, useDispatch } from "react-redux";
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch()
+  const notes = useSelector(state => state)
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const content = event.target.note.value;
+    event.target.note.value = "";
+
+    dispatch(createNote(content));
+  };
+
+  const toggleImportance = (id) => {
+    dispatch(toggleImportanceOf(id));
+  };
 
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <div>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
+
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id} onClick={() => toggleImportance(note.id)}>
+            {note.content}{" "}
+            <strong>{note.important ? "important" : "not important"}</strong>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
-}
+};
+
 
 export default App;
