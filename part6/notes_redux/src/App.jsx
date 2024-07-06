821dee17b90a0acdfa7f78006dcbbba19108084f
manav809 <manavpatel6978@gmail.com>
2024-07-06 14:18:14 -0400

    Add Notes and Note Component

--- part6/notes_redux/src/App.jsx
+++ part6/notes_redux/src/App.jsx
@@ -1,26 +1,11 @@
-import  {createNote, toggleImportanceOf} from "./reducers/actions";
-import { useSelector, useDispatch } from "react-redux";
-import NewNote from "./components/newNote";
+import NewNote from "./components/NewNote";
+import Notes from "./components/Notes";
 
 const App = () => {
-  const dispatch = useDispatch()
-  const notes = useSelector(state => state)
-
-  const toggleImportance = (id) => {
-    dispatch(toggleImportanceOf(id));
-  };
-
   return (
     <div>
       <NewNote />
-      <ul>
-        {notes.map((note) => (
-          <li key={note.id} onClick={() => toggleImportance(note.id)}>
-            {note.content}{" "}
-            <strong>{note.important ? "important" : "not important"}</strong>
-          </li>
-        ))}
-      </ul>
+      <Notes />
     </div>
   );
 };
