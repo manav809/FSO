27f8d2b400894b7d9813d282fcb36a2cc50ef06f
manav809 <manavpatel6978@gmail.com>
2024-07-06 13:51:54 -0400

    Seperare NewNote Form

--- part6/notes_redux/src/App.jsx
+++ part6/notes_redux/src/App.jsx
@@ -1,17 +1,10 @@
 import  {createNote, toggleImportanceOf} from "./reducers/actions";
 import { useSelector, useDispatch } from "react-redux";
+import NewNote from "./components/newNote";
 
 const App = () => {
   const dispatch = useDispatch()
   const notes = useSelector(state => state)
-
-  const addNote = (event) => {
-    event.preventDefault();
-    const content = event.target.note.value;
-    event.target.note.value = "";
-
-    dispatch(createNote(content));
-  };
 
   const toggleImportance = (id) => {
     dispatch(toggleImportanceOf(id));
@@ -19,11 +12,7 @@ const App = () => {
 
   return (
     <div>
-      <form onSubmit={addNote}>
-        <input name="note" />
-        <button type="submit">add</button>
-      </form>
-
+      <NewNote />
       <ul>
         {notes.map((note) => (
           <li key={note.id} onClick={() => toggleImportance(note.id)}>
