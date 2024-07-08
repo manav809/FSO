d70f11bf3dbf74859ad6bd4c50adfac08df0423c
manav809 <manavpatel6978@gmail.com>
2024-07-07 21:52:55 -0400

    Store With Complex State

--- part6/notes_redux/src/reducers/noteReducer.js
+++ part6/notes_redux/src/reducers/noteReducer.js
@@ -1,6 +1,19 @@
-const noteReducer = (state = [], action) => {
+const initialState = [
+  {
+    content: "reducer defines how redux store works",
+    important: true,
+    id: 1,
+  },
+  {
+    content: "state of store can contain any data",
+    important: false,
+    id: 2,
+  },
+];
+
+const noteReducer = (state = initialState, action) => {
   if (action.type === "NEW_NOTE") {
-    return [...state, action.payload]
+    return [...state, action.payload];
   } else if (action.type === "TOGGLE_IMPORTANCE") {
     const id = action.payload.id;
     const noteToChange = state.find((n) => n.id === id);
